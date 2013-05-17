84b9b1cb1359aa832c8e0434bfc29124a2327b46
ccpowell <chris_powell@drcog.org>
2013-05-17 11:11:16 -0600

    first release with map

---
+++ DurMap.Site/App/viewmodels/map.js
@@ -0,0 +1,30 @@
+﻿define(['durandal/app', 'durandal/system'], function (app, system) {
+    var map;
+
+    return {
+        map: map,
+        viewAttached: function (view) {
+            var div = document.getElementById("mapDiv");
+            system.log("map attaching");
+            if (map === undefined) {
+                system.log("map is not defined");
+            }
+            if (map === null) {
+                system.log("map is null");
+            }
+            if (map) {
+                system.log("map is made!");
+            } else {
+                system.log("creating map");
+                if (!div) {
+                    system.log("ERROR! could not find mapDiv");
+                }
+                map = new Microsoft.Maps.Map(div, {
+                    credentials: "AqFS5IR76tuVDO7gUl9YncTj1GHOkrJC00Ol_qz1qJNG31f4Ie9Utw8TZEF0mEIX",
+                    center: new Microsoft.Maps.Location(45.5, -122.5),
+                    zoom: 7
+                });
+            }
+        }
+    };
+});
---
+++ DurMap.Site/Controllers/DurandalController.cs
@@ -0,0 +1,9 @@
+using System.Web.Mvc;
+
+namespace DurMap.Site.Controllers {
+  public class DurandalController : Controller {
+    public ActionResult Index() {
+      return View();
+    }
+  }
+}
