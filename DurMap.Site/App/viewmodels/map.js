1ae455b27cf69f38be924310b8853330e26b4952
ccpowell <chris_powell@drcog.org>
2013-05-19 12:01:43 -0600

    use single view page

--- DurMap.Site/App/viewmodels/map.js
+++ DurMap.Site/App/viewmodels/map.js
@@ -1,30 +1,50 @@
-﻿define(['durandal/app', 'durandal/system'], function (app, system) {
-    var map;
+﻿// The following comments are for JSLint.
+// Do NOT remove them!
+// see http://www.jslint.com/
+/*jslint browser: true, debug: true, devel: true, white: true, plusplus: true, maxerr: 100, unparam: true, indent: 4 */
+/*global $: false, define: false, ko: false, Microsoft: false */
 
-    return {
-        map: map,
-        viewAttached: function (view) {
-            var div = document.getElementById("mapDiv");
-            system.log("map attaching");
-            if (map === undefined) {
-                system.log("map is not defined");
-            }
-            if (map === null) {
-                system.log("map is null");
-            }
-            if (map) {
-                system.log("map is made!");
-            } else {
-                system.log("creating map");
-                if (!div) {
-                    system.log("ERROR! could not find mapDiv");
-                }
-                map = new Microsoft.Maps.Map(div, {
-                    credentials: "AqFS5IR76tuVDO7gUl9YncTj1GHOkrJC00Ol_qz1qJNG31f4Ie9Utw8TZEF0mEIX",
-                    center: new Microsoft.Maps.Location(45.5, -122.5),
-                    zoom: 7
-                });
-            }
-        }
+define(['durandal/app', 'durandal/system', 'viewmodels/reportType'], function (app, system, rt) {
+    "use strict";
+    var self = {
+        map: null,
+        reportType: ko.observable("nothing selected")
     };
+
+    function viewAttached(view) {
+        var div = $(view).find('div.map-container').get(0);
+        system.log("map attaching");
+        if (self.map === undefined) {
+            system.log("map is not defined");
+        }
+        if (self.map === null) {
+            system.log("map is null");
+        }
+        if (self.map) {
+            system.log("map is made!");
+        } else {
+            system.log("creating map");
+            if (!div) {
+                system.log("ERROR! could not find mapDiv");
+            }
+            self.map = new Microsoft.Maps.Map(div, {
+                //AgC2Es49yUnBEY3Wev3y6qlN6MNFGYFMInS-w7MsUnKJs0gUtsvzYexB0JsvUFmr
+                //AjtUzWJBHlI3Ma_Ke6Qv2fGRXEs0ua5hUQi54ECwfXTiWsitll4AkETZDihjcfeI ISDK
+                //AqFS5IR76tuVDO7gUl9YncTj1GHOkrJC00Ol_qz1qJNG31f4Ie9Utw8TZEF0mEIX
+                //AjVI0TPKHqwalgzvnmlL3S3IC_UiHnxKcpRCNUtRNXbZenP1a-3uwADNDVJkwN3i
+                credentials: "AjtUzWJBHlI3Ma_Ke6Qv2fGRXEs0ua5hUQi54ECwfXTiWsitll4AkETZDihjcfeI",
+                center: new Microsoft.Maps.Location(40, -95),
+                zoom: 4
+            });
+        }
+    }
+
+    function getReport(x, y) {
+        system.log("get report " + rt.reportType());
+    }
+
+    return $.extend(self, {
+        getReport: getReport,
+        viewAttached: viewAttached
+    });
 });
---
+++ DurMap.Site/App/viewmodels/reportType.js
@@ -0,0 +1,24 @@
+﻿// The following comments are for JSLint.
+// Do NOT remove them!
+// see http://www.jslint.com/
+/*jslint browser: true, debug: true, devel: true, white: true, plusplus: true, maxerr: 100, unparam: true, indent: 4 */
+/*global jQuery: false, $: false, define: false, ko: false, Microsoft: false */
+
+define(['durandal/system'], function (system) {
+    "use strict";
+
+    function initializeReportTypes() {
+        var i, reports = [];
+        for (i = 0; i < 25; i++) {
+            reports.push({
+                name: i.toString()
+            });
+        }
+        return reports;
+    }
+
+    return {
+        reports: initializeReportTypes(),
+        reportType: ko.observable("nothing selected")
+    };
+});
